2fc44494469718f7270b0fcc3c9f4c76db589333
Alexander Mandrikov <mandrikov@pm.me>
2024-06-19 21:56:05 +0700

    remove annotation from UserControllerTest

--- app/src/test/java/hexlet/code/controller/api/UserControllerTest.java
+++ app/src/test/java/hexlet/code/controller/api/UserControllerTest.java
@@ -8,7 +8,6 @@ import org.instancio.Instancio;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -28,7 +27,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 class UserControllerTest {
 
     @Autowired
