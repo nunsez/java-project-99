14275244998c3765c49ec21eb6ec236b221d99e8
Alexander Mandrikov <mandrikov@pm.me>
2024-06-23 14:39:22 +0700

    add request logging

---
+++ src/main/java/hexlet/code/config/RequestLoggingFilterConfig.java
@@ -0,0 +1,21 @@
+package hexlet.code.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingFilterConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter loggingFilter() {
+        var filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10_000);
+        filter.setIncludeHeaders(true);
+        filter.setAfterMessagePrefix("REQUEST DATA: ");
+        return filter;
+    }
+
+}
